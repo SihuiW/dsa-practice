e4565d6632c69fb7063ec0b3d46d005bd73be7aa
Sihui Wang <sihui.amy.wang@gmail.com>
2026-09-09 20:41:32 +0100

    Add: climbing-stairs - submission-10

---
+++ Data Structures & Algorithms/climbing-stairs/submission-10.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 1) return 1;
+        int one = 1;
+        int two = 2;
+        for(int i = 3; i <= n; i++) {
+            int temp = two + one;
+            one = two;
+            two = temp;
+
+        }
+        return two;
+    }
+}

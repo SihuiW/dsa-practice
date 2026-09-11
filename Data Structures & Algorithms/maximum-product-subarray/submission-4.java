608cb62987cc0decadef60c0bef9b63dc5235cc5
Sihui Wang <sihui.amy.wang@gmail.com>
2026-09-11 12:36:51 +0100

    Add: maximum-product-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int[] maxDp = new int[n];
+        int[] minDp = new int[n];
+        maxDp[0] = nums[0];
+        minDp[0] = nums[0];
+        for(int i = 1; i < n; i++) {
+            maxDp[i] = Math.max(nums[i], Math.max(maxDp[i - 1] * nums[i], minDp[i - 1] * nums[i]));
+            minDp[i] = Math.min(nums[i], Math.min(maxDp[i - 1] * nums[i], minDp[i - 1] * nums[i]));
+        }
+        int res = Integer.MIN_VALUE;
+        for(int m: maxDp) {
+            res = Math.max(res, m);
+        }
+        return res;
+    }
+}
